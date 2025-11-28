41a5d421393d2f6b98ce19b25ca5ef9b6229ebaf
bread-stealer <guilhermesilvanegrinho@gmail.com>
2025-11-28 13:44:14 +0000

    Removed unrelated files from this rep

--- AI.py
+++
@@ -1,6 +0,0 @@
-# Attempting to create an AI machine with Python Language - Self Learning
-
-#1ST Step
-# Receives input from the user & prints the received input
-user_input = input("Hello! What do you want? ").strip()
-print(f"Input: {user_input}")
